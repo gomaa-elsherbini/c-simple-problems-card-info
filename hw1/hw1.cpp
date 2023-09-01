5e47261115c0dbdb0225dd457bcf529f24f8b4b7
gomaa <gogochemist35@gmail.com>
2023-09-01 17:32:44 +0300

    Add project files.

---
+++ hw1/hw1.cpp
@@ -0,0 +1,171 @@
+
+#include <iostream>
+
+void displayMyCardInfo()
+{
+    std::cout << "***************************\n";
+    std::cout << "Name: Gomaa ElSherbini\n";
+    std::cout << "Age: 39 years\n";
+    std::cout << "City: Cairo\n";
+    std::cout << "Country: Egypt\n";
+    std::cout << "***************************\n";
+}
+void printSquareStars()
+{
+    std::cout << "****************\n";
+    std::cout << "****************\n";
+    std::cout << "****************\n";
+    std::cout << "****************\n";
+}
+void iLoveProgramming()
+{
+    std::cout << "I love programming!\n\n";
+    std::cout << "I promise to be the best developer ever!\n\n";
+    std::cout << "I know it will take some time to practice,\nbut i will achieve my goal.\n\n";
+    std::cout << "Best Regards,\n";
+    std::cout << "Gomaa El Sherbini\n\n";
+}
+void printH()
+{
+    std::cout << "*           *\n";
+    std::cout << "*           *\n";
+    std::cout << "*           *\n";
+    std::cout << "*************\n";
+    std::cout << "*           *\n";
+    std::cout << "*           *\n";
+    std::cout << "*           *\n";
+}
+
+int main()
+{
+ 
+    displayMyCardInfo();
+    printSquareStars();
+    iLoveProgramming();
+    printH();
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*std::cout << "***************************\n";
+   std::cout << "Name: Gomaa ElSherbini\n";
+   std::cout << "Age: 39 years\n";
+   std::cout << "City: Cairo\n";
+   std::cout << "Country: Egypt\n";
+   std::cout << "***************************\n";*/
+
+   /*  std::cout << "****************\n";
+     std::cout << "****************\n";
+     std::cout << "****************\n";
+     std::cout << "****************\n";*/
+
+
+     /* std::cout << "I love programming!\n\n";
+      std::cout << "I promise to be the best developer ever!\n\n";
+      std::cout << "I know it will take some time to practice,\nbut i will achieve my goal.\n\n";
+      std::cout << "Best Regards,\n";
+      std::cout << "Gomaa El Sherbini\n\n";*/
+
+
+      //std::cout << "*           *\n";
+      //std::cout << "*           *\n";
+      //std::cout << "*           *\n";
+      //std::cout << "*************\n";
+      //std::cout << "*           *\n";
+      //std::cout << "*           *\n";
+      //std::cout << "*           *\n";
